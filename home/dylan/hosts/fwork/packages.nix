315dd07eb70a5f0dd98079579f7dba7533e4aed4
Dylan <dylan@cleary.org>
2025-11-06 17:26:36 -0500

    added packages

--- home/dylan/hosts/fwork/packages.nix
+++ home/dylan/hosts/fwork/packages.nix
@@ -7,16 +7,19 @@
     brave
     bitwarden-desktop
     busybox
+    cachix
     chromium
     easyeffects
     firefox
     gcc
     gnumake
+    libreoffice
     mullvad-browser
     mullvad-vpn
     newsboat
     nextcloud-client
     nodejs
+    ollama
     prismlauncher
     signal-desktop
     spotify
